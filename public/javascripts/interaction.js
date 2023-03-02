5e3798a88990d6650fda127a8d571e030c67b715
arcangel25 <arcangel25@up.edu>
2023-03-02 12:17:30 -0800

    Huh

---
+++ public/javascripts/interaction.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*code courtesy of https://alvarotrigo.com/blog/hamburger-menu-css/*/
+function menuOnClick() {
+	document.getElementById("menu-bar").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
+  function on() {
+	document.getElementById("overlay").style.display = "block";
+  }
+  function off() {
+	document.getElementById("overlay").style.display = "none";
+  } 
