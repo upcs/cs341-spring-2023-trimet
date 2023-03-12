e2f053ad23b9615f98a4c597df112c1a5f11dcce
clarkm25 <clarkm25@up.edu>
2023-03-12 14:23:35 -0700

    Made general fetchStops line for all bus/max lines

--- public/javascripts/fetch.js
+++ public/javascripts/fetch.js
@@ -116,68 +116,70 @@ function fetchAppXml(url, params, onSuccess, onError) {
 	return fetchXml(makeAppUrl(url, params), onSuccess, onError);
 }
 
-// source: https://www.w3schools.com/xml/ajax_applications.asp
-// printRedTable is identical, just for red line.
-function printBlueTable() {
-	//Grabs data from routeConfig, which "Retrieves a list of routes being reported by TransitTracker from the active schedule"
-	//Currently shows stops and direction it's going
+/**
+ * source: https://www.w3schools.com/xml/ajax_applications.asp
+ * 
+ * Gets destinations (as well as each stop on the way) for the provided bus or MAX line using the function to fetch xml data from
+ * the trimet database
+ * 
+ * @param busName Name of bus or MAX line that is to be fetched.
+ * 
+ * SIDE NOTE: Method currently puts data into a table format and prints it into the schedule div box. This will most likely be changed
+ * in the future in order to better display data based on clicks from the map.
+ */
+function fetchStops(busName) {
 	fetchAppXml("https://developer.trimet.org/ws/V1/routeConfig", {stops: true, dir: true}, function(xml) {
-		var i;
+		var i, k, highestStops, busNum;
 		var xmlDoc = xml.documentElement;
-		//dir element stands for "direction"
-		var dest = xmlDoc.getElementsByTagName("dir");
-		//Shows data in 2 columns, and aligns proper stops with the direction its going in.
-		var dest0Stops = dest[0].getElementsByTagName("stop");
-		var dest1Stops = dest[1].getElementsByTagName("stop");
-		var table = "<tr><th>" + dest[0].getAttribute("desc") + "</th><th>"
-		+ dest[1].getAttribute("desc") + "</th></tr>";
-		//Shows the Stops for the respective destination.
-		for (i = 0; i < dest0Stops.length; i++) {
-			table += "<tr><td>" + dest0Stops[i].getAttribute("desc") + "</td>";
-			table += "<td>" + dest1Stops[i].getAttribute("desc") + "</td></tr>";
+		var buses = xmlDoc.getElementsByTagName("route");
 
+		//Goes through all available bus lines/ MAX rails in order to find data for given bus/MAX line
+		for (i = 0; i < buses.length; i++) {
+			if (buses[i].getAttribute("desc") == busName) {
+				busNum = i;
+			}
+		} 
+		var destinations = buses[busNum].getElementsByTagName("dir");
+
+		var table = "<tr><th>" + destinations[0].getAttribute("desc") + "</th><th>" +
+		destinations[1].getAttribute("desc") + "</th></tr>";
+
+		
+
+		//Only 2 destinations for each of the busses/ MAX lines which is reason for hard coded 0 and 1 destinations
+		var dest0Stops = destinations[0].getElementsByTagName("stop");
+		var dest1Stops = destinations[1].getElementsByTagName("stop");
+
+		//Makes sure all stops are iterated through incase 1 destinations amount of stops is lower than another
+		if (dest0Stops.length >= dest1Stops.length) {
+			highestStops = dest0Stops.length;
 		}
-		//updates the table
-		document.getElementById("lineTable").innerHTML = table;
-		//Simply throws an error msg in the log is there's something wrong.
-	}, function(status) {
-    	console.log("We got an error: " + status);
-	});	
-}
-
-function  printRedTable() {
-	fetchAppXml("https://developer.trimet.org/ws/V1/routeConfig", {stops: true, dir: true}, function(xml) {
-		var i;
-		var xmlDoc = xml.documentElement;
-		var dest = xmlDoc.getElementsByTagName("dir");
-		var dest2Stops = dest[2].getElementsByTagName("stop");
-		var dest3Stops = dest[3].getElementsByTagName("stop");
-		var table = "<tr><th>" + dest[2].getAttribute("desc") + "</th><th>"
-		+ dest[3].getAttribute("desc") + "</th></tr>";
-
-	for (i = 0; i < dest2Stops.length; i++) {
-			table += "<tr><td>" + dest2Stops[i].getAttribute("desc") + "</td>";
-			table += "<td>" + dest3Stops[i].getAttribute("desc") + "</td></tr>";
-
+		else {
+			highestStops = dest1Stops.length;
 		}
 
+		for (k = 0; k < highestStops; k++) {
+			//If k exceeds the amount of stops for a certain destination, prints an empty square in the table to prevent
+			//an out of bounds error.
+			if ((dest0Stops.length - 1) < k) {
+				table += "<tr><td>  </td>";
+			}
+			else {
+				table += "<tr><td>" + dest0Stops[k].getAttribute("desc") + "</td>";
+			}
+
+			if ((dest1Stops.length - 1) < k) {
+				table += "<td>  </td></tr>";
+			}
+			else {
+				table += "<td>" + dest1Stops[k].getAttribute("desc") + "</td></tr>";
+			}
+		}
+
 		document.getElementById("lineTable").innerHTML = table;
 
-	}, function(status) {
+	},
+	function(status) {
 		console.log("We got an error: " + status);
-	});	
-}
-
-//Interacts with minimize/show button. Changes text on button and either hids or shows.
-//Must work on this to be exactly what group wants. ASK THE GROUP.
-function hideLineTable() {
-	//var last = $.data(txtbox_1, "last");
-	if ($('#minimizeBtn').text() == "Minimize Table"){	
-		$("#minimizeBtn").text("Show Table");
-		$("#lineTable").hide();
-	}
-	else{
-		$("#minimizeBtn").text("Minimize Table");
-		$("#lineTable").show();
-	}
+	});
 }
--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -1,48 +1,13 @@
-/*
-function printBlueTable() {
-	fetchAppXml("https://developer.trimet.org/ws/V1/routeConfig", {stops: true, dir: true}, function(xml) {
-		var i;
-		var xmlDoc = xml.documentElement;
-		var dest = xmlDoc.getElementsByTagName("dir");
-		var dest0Stops = dest[0].getElementsByTagName("stop");
-		var dest1Stops = dest[1].getElementsByTagName("stop");
-		var table = "<tr><th>" + dest[0].getAttribute("desc") + "</th><th>"
-		+ dest[1].getAttribute("desc") + "</th></tr>";
-
-		for (i = 0; i < dest0Stops.length; i++) {
-			table += "<tr><td>" + dest0Stops[i].getAttribute("desc") + "</td>";
-			table += "<td>" + dest1Stops[i].getAttribute("desc") + "</td></tr>";
-
-		}
-
-		document.getElementById("blueLine").innerHTML = table;
-
-	}, function(status) {
-    	console.log("We got an error: " + status);
-	});	
+//Interacts with minimize/show button. Changes text on button and either hids or shows.
+//Must work on this to be exactly what group wants. ASK THE GROUP.
+function hideLineTable() {
+	//var last = $.data(txtbox_1, "last");
+	if ($('#minimizeBtn').text() == "Minimize Table"){	
+		$("#minimizeBtn").text("Show Table");
+		$("#lineTable").hide();
+	}
+	else{
+		$("#minimizeBtn").text("Minimize Table");
+		$("#lineTable").show();
+	}
 }
-
-function  printRedTable() {
-	fetchAppXml("https://developer.trimet.org/ws/V1/routeConfig", {stops: true, dir: true}, function(xml) {
-		var i;
-		var xmlDoc = xml.documentElement;
-		var dest = xmlDoc.getElementsByTagName("dir");
-		var dest2Stops = dest[2].getElementsByTagName("stop");
-		var dest3Stops = dest[3].getElementsByTagName("stop");
-		var table = "<tr><th>" + dest[2].getAttribute("desc") + "</th><th>"
-		+ dest[3].getAttribute("desc") + "</th></tr>";
-
-	for (i = 0; i < dest2Stops.length; i++) {
-			table += "<tr><td>" + dest2Stops[i].getAttribute("desc") + "</td>";
-			table += "<td>" + dest3Stops[i].getAttribute("desc") + "</td></tr>";
-
-		}
-
-		document.getElementById("redLine").innerHTML = table;
-
-	}, function(status) {
-		console.log("We got an error: " + status);
-	});	
-}
-*/
-
