23ff7f53ad0e052bc60dab11e38177e5df181720
arcangel25 <97647934+arcangel25@users.noreply.github.com>
2023-03-15 22:31:01 -0700

    Fixed Menus (#17)
    Menu is once again operational

--- public/javascripts/interaction.js
+++ public/javascripts/interaction.js
@@ -6,3 +6,41 @@ function menuOnClick() {
 	document.getElementById("nav").classList.toggle("change");
 	document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+function on() {
+	document.getElementById("overlay").style.display = "block";
+}
+
+  function off() {
+	document.getElementById("overlay").style.display = "none";
+} 
+
+function exitAlertsOnClick() {
+	$('#alerts-window').hide();
+}
+
+function alertsPopupOnClick() {
+	$('#alerts-window').show();
+	$('#how-to-window').hide();
+	$('#about-window').hide();
+}
+
+function howToPopupOnClick(){
+	$('#how-to-window').show();
+	$('#alerts-window').hide();
+	$('#about-window').hide();
+}
+
+function exitHowToOnClick(){
+	$('#how-to-window').hide();
+}
+
+function aboutPopupOnClick(){
+	$('#about-window').show();
+	$('#how-to-window').hide();
+	$('#alerts-window').hide();
+}
+
+function exitAboutOnClick(){
+	$('#about-window').hide();
+}
