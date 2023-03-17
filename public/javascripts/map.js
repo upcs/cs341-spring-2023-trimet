d3f07437956fc1cc460b001e4801bd9f2a652b13
Vincent Robinson <robinsonvincent89@gmail.com>
2023-03-16 21:57:33 -0700

    Replace OpenLayers with Leaflet (#20)
    Co-authored-by: v-rob <robinsma25@up.edu>

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,16 +1,31 @@
 "use strict";
 
-var map = new ol.Map({
-	target: "map",
-	layers: [
-		new ol.layer.TileLayer({
-			source: new ol.source.XYZ({
-				url: "https://tiles.trimet.org/styles/trimet/{z}/{x}/{y}.png"
-			}),
-		}),
-	],
-	view: new ol.View({
-		center: ol.proj.fromLonLat([-122.676483, 45.523064]),
-		zoom: 10,
-	}),
+var map = L.map("map").setView([45.523064, -122.676483], 10);
+
+L.tileLayer("https://tiles.trimet.org/styles/trimet/{z}/{x}/{y}.png", {
+	maxZoom: 19
+}).addTo(map);
+
+var latlngs = [
+	[45.423064, -122.576483],
+	[45.523064, -122.676483],
+	[45.593064, -122.606483],
+];
+
+var polyline = L.polyline(latlngs, {
+	color: "blue"
+}).addTo(map);
+
+polyline.on("click", function(e) {
+	console.log("You clicked a polyline", e.target);
+});
+
+var marker = L.circleMarker([45.523064, -122.676483], {
+	radius: 4,
+	color: "green",
+	fillOpacity: 1,
+}).addTo(map);
+
+marker.on("click", function(e) {
+	console.log("You clicked a point", e.target);
 });
