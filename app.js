1952204bdbc997f5145c0d8b0f3f6d2344a2e531
v-rob <robinsma25@up.edu>
2023-03-16 21:51:52 -0700

    Replace OpenLayers with Leaflet

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use("/ol", express.static(path.join(__dirname, '/node_modules/ol')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
