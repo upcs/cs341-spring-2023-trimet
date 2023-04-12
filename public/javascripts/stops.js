173626f85371b500e49b252739fbb32e6964c588
PennyS8 <silliman25@up.edu>
2023-04-11 20:27:04 -0700

    commented stops.js

--- public/javascripts/stops.js
+++ public/javascripts/stops.js
@@ -1,5 +1,8 @@
 "use strict";
 
+//hashtable to assign colors to types of transit
+//this may be changed in favor of coloring based on which
+//routes we are comparing instead of types of transit
 var markerColors = {
 	unknown: "#000000",
 	MAX: "#D81B60",
@@ -10,14 +13,20 @@ var markerColors = {
 	AT: "#8931EF",
 };
 
-var stopsById = {};
-var stopsByOrder = [];
+var stopsById = {}; //hash table by id
+//when the hash table key is a number (like our id's)
+//they are reordered by acending key nums, which is out of order
+var stopsByOrder = []; //ordered stops list
 
+//object to hold a single transit stop and its info instead
+//of parsing the XML everytime we need a stop
 class Stop {
+	//establishes the id, the most important detail
 	constructor(id) {
 		this.id = id;
 	}
 
+	//creates the name and coordinates and map marker
 	constructRouteStops(stopNode) {
 		this.desc = stopNode.getAttribute("desc");
 		this.routes = [];
@@ -29,12 +38,13 @@ class Stop {
 
 		this.marker = L.circle(this.coords, {
 			radius: 5,
-			color: markerColors.unknown,
-			fillColor: markerColors.unknown,
+			color: markerColors.unknown, //is defined in consctuctMapData()
+			fillColor: markerColors.unknown, //is defined in consctuctMapData()
 			fillOpacity: 1,
 		});
 	}
 
+	//adds color to map icon
 	constructMapData(placemarkNode) {
 		let typeNode = placemarkNode.querySelector("[name='type'] > value");
 		let color = markerColors[typeNode.textContent];
@@ -45,34 +55,43 @@ class Stop {
 		});
 	}
 
+	//parent reference to all routes this stop is on
 	constructParentRoute(route) {
 		this.routes.push(route);
 	}
 
+	//visualize marker to map
 	showMarker() {
 		this.marker.addTo(map);
 	}
 
+	//remove marker from map
 	hideMarker() {
 		this.marker.remove();
 	}
 }
 
+//runs after XML files have been fetched
+//parses the XML files into stop objects
 function createStops(data) {
+	//loops thorugh routeStops XML file
 	let stopNodes = data.routeStops.querySelectorAll("stop");
 	for (let stopNode of stopNodes) {
 		let id = stopNode.getAttribute("locid");
+		//prevent duplicate stops (which do exist in the XML files)
 		if (id in stopsById) {
 			continue;
 		}
 
 		let stop = new Stop(id);
-		stopsById[id] = stop;
-		stopsByOrder.push(stop);
+		stopsById[id] = stop; //adds stop to hash table
+		stopsByOrder.push(stop); //adds stop to list
 
+		//creates name, coordinates, and marker
 		stop.constructRouteStops(stopNode);
 	}
 
+	//loops through stopCoords XML file
 	let placemarkNodes = data.stopCoords.querySelectorAll("Placemark");
 	for (let placemarkNode of placemarkNodes) {
 		let id = placemarkNode.querySelector("[name='stop_id'] > value").textContent;
@@ -85,6 +104,10 @@ function createStops(data) {
 	}
 }
 
+//there are two XML files that hold all the info we need to
+//create a stop object: routeStops and stopCoords
+
+//fetch stop list
 staticFetch.addData("routeStops",
 	() => fetchAppXml("https://developer.trimet.org/ws/V1/routeConfig", {
 		stops: true,
@@ -92,10 +115,12 @@ staticFetch.addData("routeStops",
 	})
 );
 
+//fetch stop coordinates
 staticFetch.addData("stopCoords",
 	() => fetchXml("https://developer.trimet.org/gis/data/tm_stops.kml")
 );
 
+//once they have both been fetched create the stop objects
 staticFetch.onFetch(data => {
 	createStops(data);
 });
