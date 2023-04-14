6bc4209f6dbd6c22a4efe07e08a785c92e6e05c3
arcangel25 <arcangel25@up.edu>
2023-04-13 23:44:49 -0700

    Bus Locations Updating

--- public/javascripts/busLocation.js
+++ public/javascripts/busLocation.js
@@ -1,8 +1,37 @@
 "use strict";
 
+const dotArr = [];
+
 function createBuses(){
-	console.log("Hi");
-	console.log(fastFetch.data.busLocations);
+	const arr =[];
+
+	let locs = fastFetch.data.busLocations;
+	
+	let x = locs['resultSet']['vehicle'];
+
+	for(let i = 0; i < (x.length); (i+=2)){
+		arr.push(locs['resultSet']['vehicle'][i]['latitude']);
+		arr.push(locs['resultSet']['vehicle'][i]['longitude']);
+
+		console.log(arr[i]);
+		console.log(arr[i+1]);
+
+		var circle = L.circle([arr[i], arr[i+1]], {
+			color: 'black',
+			fillColor: '#000000',
+			fillOpacity: 1,
+			radius: 30
+		}).addTo(map);
+
+		dotArr.push(circle);
+	}
+
+}
+	
+function clearBuses(arr){
+	for(let i = 0; i < (arr.length); i++){
+		arr[i].remove();
+	}
 }
 
 //The locations should be updated oftern 
@@ -10,6 +39,7 @@ fastFetch.addData("busLocations",
 	() => fetchAppJson("https://developer.trimet.org/ws/v2/vehicles")
 );
 
-staticFetch.onFetch(data => {
+fastFetch.onFetch(data => {
+	clearBuses(dotArr);
 	createBuses(data);
 });
