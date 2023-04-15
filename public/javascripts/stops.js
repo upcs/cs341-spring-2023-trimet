ee8c9b172688ba62ce86201ff0b3ddb303958217
PennyS8 <120285207+PennyS8@users.noreply.github.com>
2023-04-15 14:56:30 -0700

    Silliman25 commented routes.js and stops.js (#31)
    * commented stops.js
    
    * commented routes.js
    
    ---------
    
    Co-authored-by: clarkm25 <97877032+clarkm25@users.noreply.github.com>

--- public/javascripts/stops.js
+++ public/javascripts/stops.js
@@ -1,5 +1,8 @@
 "use strict";
 
+//hashtable to assign colors to types of transit
+//this may be changed in favor of coloring based on which
+//routes we are comparing instead of types of transit
 var markerColors = {
 	unknown: "#000000",
 	MAX: "#D81B60",
@@ -10,18 +13,26 @@ var markerColors = {
 	AT: "#8931EF",
 };
 
-var stopsById = {};
-var stopsByOrder = [];
+var stopsById = {}; //hash table by id
+//when the hash table key is a number (like our id's)
+//they are reordered by acending key nums, which is out of order
+var stopsByOrder = []; //ordered stops list
 
+//object to hold a single transit stop and its info instead
+//of parsing the XML everytime we need this stop
 class Stop {
+	//establishes the id, the most important detail
 	constructor(id) {
 		this.id = id;
 	}
 
+	//creates the name, coordinates and map marker
 	constructRouteStops(stopNode) {
 		this.desc = stopNode.getAttribute("desc");
-		this.routes = [];
-		this.routeButtons = [];
+		this.routes = []; //list of parent route references
+    
+    //list of buttons for the routes that go through a given stop
+    this.routeButtons = [];
 
 		this.coords = [
 			parseFloat(stopNode.getAttribute("lat")),
@@ -30,12 +41,13 @@ class Stop {
 
 		this.marker = L.circle(this.coords, {
 			radius: 5,
-			color: markerColors.unknown,
-			fillColor: markerColors.unknown,
+			color: markerColors.unknown, //is defined in consctuctMapData()
+			fillColor: markerColors.unknown, //is defined in consctuctMapData()
 			fillOpacity: 1,
 		});
 	}
 
+	//adds color to map icon
 	constructMapData(placemarkNode) {
 		let typeNode = placemarkNode.querySelector("[name='type'] > value");
 		let color = markerColors[typeNode.textContent];
@@ -50,7 +62,8 @@ class Stop {
 		this.button = $("<button>")
 			.text(this.desc);
 	}
-
+  
+  //parent reference to all routes this stop is on
 	constructParentRoute(route) {
 		this.routes.push(route);
 
@@ -59,30 +72,38 @@ class Stop {
 		this.routeButtons.push(routeButton);
 	}
 
+	//visualize stop marker on map
 	showMarker() {
 		this.marker.addTo(map);
 	}
 
+	//remove stop marker from map
 	hideMarker() {
 		this.marker.remove();
 	}
 }
 
+//runs after XML files have been fetched
+//parses the XML files into stop objects
 function createStops(data) {
+	//loops through routeStops XML file
 	let stopNodes = data.routeStops.querySelectorAll("stop");
 	for (let stopNode of stopNodes) {
 		let id = stopNode.getAttribute("locid");
+		//prevent duplicate stops (which do exist in the XML files)
 		if (id in stopsById) {
 			continue;
 		}
 
 		let stop = new Stop(id);
-		stopsById[id] = stop;
-		stopsByOrder.push(stop);
+		stopsById[id] = stop; //adds stop to hash table
+		stopsByOrder.push(stop); //adds stop to ordered list
 
+		//creates the name, coordinates and map marker
 		stop.constructRouteStops(stopNode);
 	}
 
+	//loops through stopCoords XML file
 	let placemarkNodes = data.stopCoords.querySelectorAll("Placemark");
 	for (let placemarkNode of placemarkNodes) {
 		let id = placemarkNode.querySelector("[name='stop_id'] > value").textContent;
@@ -99,6 +120,10 @@ function createStops(data) {
 	}
 }
 
+//there are two XML files that hold all the info we need to
+//create a stop object: routeStops and stopCoords
+
+//fetch stop list
 staticFetch.addData("routeStops",
 	() => fetchAppXml("https://developer.trimet.org/ws/V1/routeConfig", {
 		stops: true,
@@ -106,10 +131,12 @@ staticFetch.addData("routeStops",
 	})
 );
 
+//fetch stop coordinates
 staticFetch.addData("stopCoords",
 	() => fetchXml("https://developer.trimet.org/gis/data/tm_stops.kml")
 );
 
+//once they have both been fetched create the stop objects
 staticFetch.onFetch(data => {
 	createStops(data);
 });
